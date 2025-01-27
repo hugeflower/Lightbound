d758663ec8d47508c1be1ebb5e53faf5ff3f429c
Hugo Lafleur <gohu.ruelfal88@gmail.com>
2025-01-26 21:17:50 -0500

    frontend vite backend spring

---
+++ Lightbound-backend/src/test/java/com/example/lightboundbackend/LightboundBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lightboundbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LightboundBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
